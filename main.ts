fcf2001b7dabe15351147b2e258818a5dd5bd941
Mark Lee <mark@protopie.io>
2021-07-13 04:29:39 +0900

    3-4) 라우팅 설정하기

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
+import notFound from "./not_found.ts";
+
+const port = 5000;
+
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(notFound);
+
+await app.listen({ port });
