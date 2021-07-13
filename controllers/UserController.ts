c939fcdf7162f6a24fe24e0b8a078d72660c93e8
Mark Lee <mark@protopie.io>
2021-07-13 13:55:56 +0900

    3-6) 로그인 처리하기

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,7 +1,29 @@
-import { RouterContext } from "https://deno.land/x/oak/mod.ts";
+import { RouterContext, BodyForm } from "https://deno.land/x/oak/mod.ts";
+import { getUserByEmail } from "../models/User.ts";
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 
 export default class UserController {
-  public login(context: RouterContext) {
+  public async login(context: RouterContext) {
+    const { type, value } = (await context.request.body()) as BodyForm;
+    const params = await value;
+
+    const email = params.get("email");
+    const password = params.get("password");
+
+    console.log(email, password); // 2woongjae@gmail.com deno1234
+
+    if (email === null || password === null) {
+      context.response.status = 422;
+      context.response.body = { error: "email or password required" };
+      return;
+    }
+
+    const user = await getUserByEmail(email);
+    console.log(user);
+
+    const match = await bcrypt.compare(password, user.password);
+    console.log(match);
+
     context.response.body = "login";
   }
 
---
+++ models/User.ts
@@ -0,0 +1,19 @@
+import client from "./config.ts";
+
+export interface User {
+  userId: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export async function getUserByEmail(email: string): Promise<User> {
+  const { rows } = await client.execute(
+    `SELECT * FROM Users WHERE email = '${email}'`
+  );
+  if (rows === undefined) throw new Error();
+  if (rows.length !== 1) throw new Error();
+  return rows[0];
+}
